20b2d090a6fff759f218855e4f3c9566f186ab0d
Didrik Molinder <didrik.molinder@protonmail.com>
2024-09-19 10:21:28 +0200

    refactored Program.cs

--- lexicon-basic-oop/Program.cs
+++ lexicon-basic-oop/Program.cs
@@ -5,18 +5,55 @@ public class Program()
     public static void Main()
     {
         Console.ForegroundColor = ConsoleColor.Green;
+
         //Person p = new Person { FirstName = "Didrik", LastName = "Molinder" };
         //p.fName = "didrik";
         //Går inte eftersom private
+
         try
         {
             Person newPerson = PersonHandler.CreatePerson(35, "Didrik", "Molinder", 188, 75);
             System.Console.WriteLine(newPerson.ToString());
             PersonHandler.SetAge(newPerson, 36);
             System.Console.WriteLine(newPerson.ToString());
-            //Person badPerson = PersonHandler.CreatePerson(0, "Some", "Guy", 0, 0);//För att testa exception handling
             PersonHandler.Operate(newPerson);
             System.Console.WriteLine(newPerson.ToString());
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            System.Console.WriteLine($"Exception occurred: {ex.Message}");
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+        try
+        {
+            Person badPerson = PersonHandler.CreatePerson(0, "Some", "Guy", 0, 0);
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            System.Console.WriteLine($"Exception occurred: {ex.Message}");
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+        try
+        {
+            List<UserError> userErrors = new();
+            userErrors.Add(new NumericInputError());
+            userErrors.Add(new TextInputError());
+
+            foreach (var error in userErrors)
+            {
+                System.Console.WriteLine(error.UEMessage());
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            System.Console.WriteLine($"Exception occurred: {ex.Message}");
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+        try
+        {
             Horse h = new("Dusty", 8, 400, "Palomino");
             h.DoSound();
             h.Prance();
@@ -27,15 +64,6 @@ public class Program()
             Console.ForegroundColor = ConsoleColor.Red;
             System.Console.WriteLine($"Exception occurred: {ex.Message}");
             Console.ForegroundColor = ConsoleColor.Green;
-        }
-
-        List<UserError> userErrors = new();
-        userErrors.Add(new NumericInputError());
-        userErrors.Add(new TextInputError());
-
-        foreach (var error in userErrors)
-        {
-            System.Console.WriteLine(error.UEMessage());
         }
     }
 }
